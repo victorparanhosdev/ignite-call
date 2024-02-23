6080eafdde050ece59cd8f44544787bac70a8e42
Victor Paranhos <victor_paranhos@hotmail.com>
2024-02-22 22:45:56 -0300

    permissions

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,18 +1,20 @@
 import { globalStyles } from "../styles/global";
-
+import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps: {session, ...pageProps }}: AppProps) {
   return (
-    <>
+  
+    <SessionProvider session={session}>
     <Head>
       <title>Ignite Call</title>
     </Head>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
+    </SessionProvider>
     
-    </>
   )
 }
+
