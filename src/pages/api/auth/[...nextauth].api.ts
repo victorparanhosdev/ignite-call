fd9d49087be9652059a611fc7004e212945637ec
Victor Paranhos <victor_paranhos@hotmail.com>
2024-02-23 22:14:56 -0300

    adapter

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,7 +1,8 @@
+import { PrismaAdapter } from '@/src/lib/auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 export const authOptions: NextAuthOptions = {
-
+  adapter: PrismaAdapter(),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
