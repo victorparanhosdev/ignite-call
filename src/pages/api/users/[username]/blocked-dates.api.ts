a5e12af7e379240b453fc57d3e490b216911d356
Victor Paranhos <victor_paranhos@hotmail.com>
2024-03-12 19:35:19 -0300

    Update blocked-dates.api.ts

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -42,7 +42,11 @@ export default async function handler(
     )
   })
 
+  const blockedDateRaw = await prisma.$queryRaw`
+    SELECT * FROM schedulings S
+    WHERE S.user_id = ${user.id} AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+  
+  `
 
-
-  return res.json({ blockedWeekDays})
+  return res.json({ blockedWeekDays, blockedDateRaw})
 }
