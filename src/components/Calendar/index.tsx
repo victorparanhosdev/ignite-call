b9380a5c3eb3711712ad942a17029df575e59f63
Victor Paranhos <victor_paranhos@hotmail.com>
2024-03-12 21:06:07 -0300

    calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -28,6 +28,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -67,7 +68,7 @@ export function Calendar({selectedDate, onDateSelected}: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
          year: currentDate.get('year'),
-         month: currentDate.get('month')
+         month: currentDate.get('month') + 1
         },
       })
 
@@ -116,7 +117,7 @@ export function Calendar({selectedDate, onDateSelected}: CalendarProps) {
       ...daysInMonthArray.map((date) => {
         return {
           date,
-          disabled: date.endOf('day').isBefore(new Date()) || blockedDates.blockedWeekDays.includes(date.get('day'))
+          disabled: date.endOf('day').isBefore(new Date()) || blockedDates.blockedWeekDays.includes(date.get('day')) || blockedDates.blockedDates.includes(date.get('date'))
         }
       }),
       ...nextMonthFillArray.map((date) => {
